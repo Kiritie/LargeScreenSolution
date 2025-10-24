3e9e8fb894fc12ace5fe205917a9e3fc12ee013e
wuhao <810238014@qq.com>
2025-10-24 15:23:30 +0800

    添加插件

--- Source/LargeScreenSolution/Private/Message/Sound/MessageHandle_PlaySound.cpp
+++ Source/LargeScreenSolution/Private/Message/Sound/MessageHandle_PlaySound.cpp
@@ -4,7 +4,9 @@
 
 #include "ZMQControlTypes.h"
 #include "Components/AudioComponent.h"
+#include "Kismet/GameplayStatics.h"
 #include "Scene/SceneManager.h"
+#include "Scene/Entity/EntityBase.h"
 
 UMessageHandle_PlaySound::UMessageHandle_PlaySound()
 {
@@ -21,18 +23,45 @@ void UMessageHandle_PlaySound::OnReceiveMessage(const FString& InData)
 	if (FJsonSerializer::Deserialize(Reader, JsonObject))
 	{
 		FString ID = TEXT("");
+		FString EntityID = TEXT("");
+		USoundBase* Sound = nullptr;
+		float Volume = 0.f;
 		
 		if(JsonObject->HasField(TEXT("id")))
 		{
 			ID = JsonObject->GetStringField(TEXT("id"));
 		}
-
-		UAudioComponent* AudioComponent = ASceneManager::Get()->GetSceneObject<UAudioComponent>(ID);
-		if(AudioComponent)
+		if(JsonObject->HasField(TEXT("entity_id")))
 		{
-			AudioComponent->DestroyComponent();
+			EntityID = JsonObject->GetStringField(TEXT("entity_id"));
+		}
+		if(JsonObject->HasField(TEXT("sound")))
+		{
+			const FString Field = JsonObject->GetStringField(TEXT("sound"));
+			Sound = LoadObject<USoundBase>(nullptr, *Field);
+		}
+		if(JsonObject->HasField(TEXT("volume")))
+		{
+			Volume = JsonObject->GetNumberField(TEXT("volume"));
+		}
 
-			ASceneManager::Get()->RemoveSceneObject(ID);
+		if(Sound)
+		{
+			UAudioComponent* AudioComponent = nullptr;
+			if(EntityID.IsEmpty())
+			{
+				AudioComponent = UGameplayStatics::SpawnSound2D(this, Sound, Volume, 1.f, 0.f, nullptr, nullptr, false);
+			}
+			else
+			{
+				AEntityBase* Entity = ASceneManager::Get()->GetSceneObject<AEntityBase>(EntityID);
+				if(Entity)
+				{
+					AudioComponent = UGameplayStatics::SpawnSoundAttached(Sound, Entity->GetRootComponent(), NAME_None, FVector::ZeroVector, FRotator::ZeroRotator, EAttachLocation::KeepRelativeOffset, false, Volume, 1.f, 0.f, nullptr, nullptr, false);
+				}
+			}
+
+			ASceneManager::Get()->AddSceneObject(ID, AudioComponent);
 		}
 	}
 }
